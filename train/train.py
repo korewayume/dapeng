d0853bf05435383d3fa542d08ab15b77d5414fd9
zhoujia <korewayume@foxmail.com>
2017-12-12 15:55:54 +0800

    更新训练脚本

--- train/train.py
+++ train/train.py
@@ -13,8 +13,7 @@ with open("u-net-train.json") as cfg:
 unet_model_path = configuration.get("unet_model_path", "ModelArchive/u-net-model.yaml")
 unet_weight_path = configuration.get("unet_weight_path", 'ModelArchive/unet_best_checkpoint.hdf5')
 unet_weight_checkpoint_path = configuration.get("unet_weight_checkpoint_path", 'ModelArchive/unet_best_checkpoint.hdf5')
-tensorboard_log_directory = os.path.join(configuration["tensorboard_log_directory"],
-                                         datetime.now().strftime("%m-%d-%H-%M"))
+tensorboard_log_directory = os.path.join(configuration["tensorboard_log_directory"], datetime.now().strftime("%m-%d-%H-%M"))
 load_weights = configuration.get("load_weights", False)
 fit_verbose = configuration.get("fit_verbose", True)
 mosaic_size = configuration.get("mosaic_size", 256)
@@ -27,6 +26,8 @@ train_y_path = configuration["train_y_path"]
 valid_x_path = configuration["valid_x_path"]
 valid_y_path = configuration["valid_y_path"]
 valid_batch_size = configuration.get("valid_batch_size", 10)
+random_rotate_angle = configuration.get("random_rotate_angle", 15)
+random_rotate_mode = configuration.get("random_rotate_mode", "reflect")
 
 
 model = unet5(
@@ -46,7 +47,7 @@ filename_regex = re.compile(r"^\d{3}_\d{3}_\d{3}\.tif")
 print("正在扫描训练样本")
 train_gen = train_from_directories(train_x_path, train_y_path, x_tif_reader, y_tif_reader,
                                    filename_regex, batch_size=batch_size,
-                                   random_transform_func=random_transform(30))
+                                   random_transform_func=random_transform(random_rotate_angle, mode=random_rotate_mode))
 print("找到训练样本：{}".format(len(train_gen)))
 print("正在扫描验证样本")
 valid_gen = train_from_directories(valid_x_path, valid_y_path, x_tif_reader, y_tif_reader,
